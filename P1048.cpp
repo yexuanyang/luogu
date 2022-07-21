0f1f761ee88a67c3e0f3ca5502e474748c439aa7
yyx <1182282462@qq.com>
2022-07-21 17:05:59 +0800

    背包问题

--- P1048.cpp
+++ P1048.cpp
@@ -1,58 +1,29 @@
+//这题采药时间就是背包的容积，药品的采取时间就是weight，价值就是value，问题转换成背包问题
 #include <iostream>
-#include <algorithm>
-#define MAXNUM 1000
+#include <vector>
+#define max(n, m) n > m ? n : m
 using namespace std;
-struct Medicine
-{
-    int time, value;
-    double tDivV;
-    bool operator<(const Medicine &m)
-    {
-        return tDivV > m.tDivV;
-    }
-} medicine[MAXNUM];
 int main()
 {
     int t, m;
     cin >> t >> m;
-    for (int i = 0; i < m; i++)
+    vector<int> weight{0};
+    vector<int> value{0};
+    for (int i = 1; i <= m; i++)
     {
-        int time, v;
-        cin >> time >> v;
-        medicine[i].time = time;
-        medicine[i].value = v;
-        medicine[i].tDivV = v * 1.0 / time;
+        int time, va;
+        cin >> time >> va;
+        weight.push_back(time);
+        value.push_back(va);
     }
-    sort(medicine, medicine + m);
-    /*for (int i = 0; i < m; i++)
+    vector<int> f(t + 1, 0);
+    for (int i = 1; i <= m; i++)
     {
-        cout << medicine[i].tDivV << endl;
-    }*/
-    int max = 0;
-    for (int i = 0; i < m; i++)
-    {
-        int temp = t;
-        int ans = 0;
-        if (medicine[i].time <= temp)
+        for (int j = t; j >= weight[i]; j--)
         {
-            temp -= medicine[i].time;
-            ans += medicine[i].value;
-            for (int j = 1; j <= m - i; j++)
-            {
-                for (int k = i + j; k < m; k++)
-                {
-                    if (medicine[k].time <= temp)
-                    {
-                        temp -= medicine[k].time;
-                        ans += medicine[k].value;
-                    }
-                }
-            }
-        }
-        if (ans > max)
-        {
-            max = ans;
+            f[j] = max(f[j], f[j - weight[i]] + value[i]);
         }
     }
-    cout << max << endl;
+    cout << f[t];
+    return 0;
 }
